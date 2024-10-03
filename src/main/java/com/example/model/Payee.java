568e3c01f017d040c8c300c8b74f7577683e0d78
SoumickSaha <ssaha2009@gmail.com>
2024-10-02 20:05:45 -0500

    Model updated without lombok

--- src/main/java/com/example/model/Payee.java
+++ src/main/java/com/example/model/Payee.java
@@ -8,30 +8,67 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 import io.micronaut.core.annotation.Introspected;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Introspected
 @Entity
 @Table(name = "payees")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Payee {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @NotBlank
-    private String payeeName;
+	@NotBlank
+	private String payeeName;
 
-    @NotBlank
-    private String accountNumber;
+	@NotBlank
+	private String accountNumber;
 
-    @NotBlank
-    private String address;
+	@NotBlank
+	private String address;
 
-   
+	public Payee() {
+		super();
+	}
+
+	public Payee(Long id, @NotBlank String payeeName, @NotBlank String accountNumber, @NotBlank String address) {
+		super();
+		this.id = id;
+		this.payeeName = payeeName;
+		this.accountNumber = accountNumber;
+		this.address = address;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPayeeName() {
+		return payeeName;
+	}
+
+	public void setPayeeName(String payeeName) {
+		this.payeeName = payeeName;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 }
